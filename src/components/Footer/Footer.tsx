82c77713e63ba2c4fc33e3c69ce188950f9107e7
akshayn10 <thiruakshayan98@gmail.com>
2022-09-09 13:57:29 +0530

    Footer changes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,63 @@
-import './Footer.css'
+import "./Footer.css";
+import logo from "../../assets/img/logo/logo.png";
+import { FaFacebook } from "react-icons/fa";
+import { AiOutlineYoutube } from "react-icons/ai";
+import { MdOutlineCopyright } from "react-icons/md";
 const Footer = () => {
-    return (
-        <div className="footer-container">
-            Footer
+  return (
+    <div className="footer-container">
+      <div className="footer-flex">
+        <div>
+          <div className="footer-logo">
+            <img src={logo} alt="" />
+            <h2>TDUSA</h2>
+          </div>
+          <div>
+            <p>STUDIES DETERMINE FUTURE</p>
+            <p className="address-container">
+              Town Hall Premises, Dockyard Road
+              <br />
+              Trincomalee
+              <br />
+              Sri Lanka
+            </p>
+          </div>
         </div>
-    )
-}
+        <div>
+          <div className="item-title">
+            <p>GET IN TOUCH</p>
+            <div></div>
+          </div>
+          <div className="contact-section">
+            <h3>E-MAIL US</h3>
+            <p>tdusasl@gmail.com</p>
+            <h3>CALL US</h3>
+            <p>+94 760075370</p>
+          </div>
+        </div>
+        <div>
+          <div className="item-title">
+            <p>FOLLOW</p>
+            <div></div>
+          </div>
+          <div className="social-media-container">
+            <FaFacebook />
+            <AiOutlineYoutube />
+          </div>
+          <div className="item-title review-section">
+            <p>GIVE A REVIEW</p>
+            <div></div>
+          </div>
+        </div>
+      </div>
+      <div className="copyright-section">
+        <MdOutlineCopyright className="copyright" />
+        <div>
+          All Rights Reserved By Trincomalee District University Students'
+          Association. 2022
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Footer;
---
+++ src/components/Magazine/Magazine.tsx
@@ -0,0 +1,10 @@
+import './Magazine.css';
+
+const Magazine = () => {
+    return (
+        <div>
+        <h1>Magazine</h1>
+        </div>
+    );
+}
+export default Magazine;
